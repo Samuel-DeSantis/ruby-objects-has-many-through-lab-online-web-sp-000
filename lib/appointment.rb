d95512097c73107f94f2a95fc7270d8cc1c9a88e
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-12-15 05:58:14 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,3 +1,17 @@
 class Appointment
+  attr_accessor :patient, :doctor
+
+  @@all = []
+
+  def initialize(date, patient, doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
+    save
+  end
+
+  def save
+    @@all << self
+  end
 
 end
