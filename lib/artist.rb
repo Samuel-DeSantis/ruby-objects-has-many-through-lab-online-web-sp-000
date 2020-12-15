30ef4b9ad4e0a36a3d4479070492a9599649fe5a
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-12-15 05:52:24 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,3 +1,22 @@
 class Artist
+  attr_accessor :name
 
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def all
+    @@all
+  end
+
+  def new_song(name, genre)
+    song = Song.new(name, genre)
+  end
 end
