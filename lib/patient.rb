9d801170f5bd6d7e3b715795cbdeb32bb8442591
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-12-15 06:39:51 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -14,14 +14,6 @@ class Patient
     @@all
   end
 
-  def new_appointment(date, doctor)
-    Appointment.new(date, self, doctor)
-  end
-
-  def appointments
-    Appointment.all.select { |appt| appt.patient == self }
-  end
-
   def doctors
     Appointment.all.select { |appt| appt.doctor if appt.patient == self }
   end
