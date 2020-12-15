adfe0ff8d461072c56751acad737a6e70eea1759
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-12-15 06:26:07 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,9 +1,20 @@
 class Genre
   attr_accessor :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    save
   end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  ends
 
   def songs
     Song.all.select { |song| song.genre == self }
